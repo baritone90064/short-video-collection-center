a0fe067ad00c46bb4cfda4bc98c3fcfead7aab86
baritone90064 <baritone90064@gmail.com>
2026-07-22 15:42:54 +0800

    Update config.js

--- config.js
+++ config.js
@@ -5,9 +5,15 @@ export const CONFIG = {
     projectId: "short-video-collection-c-1940a",
     storageBucket: "short-video-collection-c-1940a.firebasestorage.app",
     messagingSenderId: "774442944506",
-    appId: "1:774442944506:web:54afea7cb09ca94ce4c643" },
+    appId: "1:774442944506:web:54afea7cb09ca94ce4c643"
+  },
   googleMapsApiKey: "AIzaSyA4-xRZrDzb63hj6nmhRjQjkerd2Dmlho0",
   googleMapsMapId: "d4056d4f23c4e1a4fb2b395b",
+  sharedWorkspaceId: "shared",
+  allowedEmails: [
+    "baritone90064@gmail.com",
+    "terry4410@gmail.com"
+  ],
   appName: "短影音收藏中心",
   defaultCenter: { lat: 25.0478, lng: 121.5319 },
   defaultRadiusKm: 20,

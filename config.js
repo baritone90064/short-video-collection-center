98924ca4a9a37a12dd7fcb902c97dac8e71ed2d2
baritone90064 <baritone90064@gmail.com>
2026-07-22 15:04:12 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+export const CONFIG = {
+  firebase: {
+    apiKey: "AIzaSyBRJy9UUaSyXQFlAgb9_alRNW0HplS5QUg",
+    authDomain: "short-video-collection-c-1940a.firebaseapp.com",
+    projectId: "short-video-collection-c-1940a",
+    storageBucket: "short-video-collection-c-1940a.firebasestorage.app",
+    messagingSenderId: "774442944506",
+    appId: "1:774442944506:web:54afea7cb09ca94ce4c643" },
+  googleMapsApiKey: "AIzaSyA4-xRZrDzb63hj6nmhRjQjkerd2Dmlho0",
+  googleMapsMapId: "d4056d4f23c4e1a4fb2b395b",
+  appName: "短影音收藏中心",
+  defaultCenter: { lat: 25.0478, lng: 121.5319 },
+  defaultRadiusKm: 20,
+  pageSize: 500
+};
